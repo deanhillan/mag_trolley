a01d5b3b5b321b1e75c4b508fb703f5046facfbb
dinob0t <dean.hillan@gmail.com>
2014-10-10 11:02:15 +1100

    Updates

--- README.md
+++ README.md
@@ -1,29 +1,41 @@
 ## Magnetic Trolley Data Processing
 
 ### Introduction
-This script is for combining the exported output of the Bartington Spectramag program, in which two input channels exist:
+The script `combined_data.m` is for combining the exported output of the Bartington Spectramag program, in which two input channels exist:
 Input 1. The 3 components of the magnetic field as a function of time
 Input 2. The potentiometer reading (related to distance) as a function of time
 
-This script writes out the magnetic field data as a function of distance into a single specified output file. There is also an option to include a set of 'background' measurements which are subtracted off the recorded magnetic field. This last mode of operation requires some interpolation between the two runs, and as such it is suggested that the trolley be run over the same distance for each set of measurements, and only be run in one direction per run.
+The script `process_single_file.m` works in almost an identical way to the above script, but is for when all 6 channels are output by the Bartington Spectramag program at once.
+
+These scripts write out the magnetic field data as a function of distance into a single specified output file. There is also an option to include a set of 'background' measurements which are subtracted off the recorded magnetic field. This last mode of operation requires some interpolation between the two runs, and as such it is suggested that the trolley be run over the same distance for each set of measurements, and only be run in one direction per run.
 
 ### Input for a non-background run
 The following files must exist and variables set:
-
+`combined_data.m` - 
 * Two unaltered (raw files include headers) '.Dat' files from each of the Bartington Spectramag inputs are required.
 * `filename_mag=` - Input 1.
 * `filename_dist=` - Input 2.
+* `output_file=` - Output file.
+
+`process_single_file.m` - 
+* One unaltered (raw file includes headers) '.Dat' files from the Bartington Spectramag input is required.
+* `filename_mag=` - Input 1.
 * `output_file=` - Output file.
 
 Note that input file names may be absolute paths rather than relative, e.g., `H:\Lindfield 2014_09_30\data1.Dat`
 
 ### Input for a background run
 Everything as per above is required but additionally:
-
+`combined_data.m` - 
 * Two '.Dat' files which specify a background trolley run.
 * `background_exist=1`
 * `filename_mag_back=` - Background Input 1.
 * `filename_dist_back=` - Background Input 2.
+
+`process_single_file.m` - 
+* One '.Dat' file which specifies a background trolley run.
+* `background_exist=1`
+* `filename_mag_back=` - Background Input 1.
 
 ### Output for a non-background run
 The output file writes 4 columns, the distance in arbitrary units, and the X, Y, and Z magnetic components in nT (`dist xnT ynT znT`).
---
+++ process_single_file.m
@@ -0,0 +1,132 @@
+clear all
+tic;
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+% For processing the output of the Bartington Spectramag export data
+% Change the filenames or (absolute) paths to filenames below
+% An absolute path might look like -
+% 'H:\Lindfield 2014_09_30\data1.Dat'
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+% Input 1: Magnetic channel
+filename_mag = 'data_single.Dat';
+% Input 2: Distance channel
+% Output file to write to
+output_file = 'combined_data.dat';
+
+% Background signal information
+% Does background data exist (1=Yes, 0=No)
+background_exist = 0;
+% Background Input 1: Magnetic channel
+filename_mag_back = 'back_single.Dat';
+% Background Input 2: Distance channel
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+%Begin
+delimiter = '\t';
+startRow = 3;
+%% Format string for each line of text:
+formatSpec_mag = '%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%[^\n\r]';
+%% Open the text file.
+disp('Attempting to open data files:')
+fileID_mag = fopen(filename_mag,'r');
+
+if (background_exist == 1)
+    disp('Attempting to open background data files:')
+    fileID_mag_back = fopen(filename_mag_back,'r');
+
+end
+    
+%% Read columns of data according to format string.
+dataArray_mag = textscan(fileID_mag, formatSpec_mag, 'Delimiter', ...
+    delimiter, 'EmptyValue' ,NaN,'HeaderLines' ,startRow-1, ...
+    'ReturnOnError', false);
+%% Close the text file.
+fclose(fileID_mag);
+if (background_exist == 1)
+    dataArray_mag_back = textscan(fileID_mag_back, formatSpec_mag, ...
+        'Delimiter', delimiter, 'EmptyValue' ,NaN,'HeaderLines' , ...
+        startRow-1, 'ReturnOnError', false);
+
+    fclose(fileID_mag_back);
+end
+%% Allocate imported array to column variable names
+Times = dataArray_mag{:, 1};
+xnT = dataArray_mag{:, 2};
+ynT = dataArray_mag{:, 3};
+znT = dataArray_mag{:, 4};
+dist = dataArray_mag{:, 12};
+
+max_dist_diff = max(dist - [dist(2:size(dist,1)); dist(size(dist,1))]);
+
+if (background_exist==0)
+    fileID_write = fopen(output_file,'w+');
+    dlmwrite(output_file,[dist xnT ynT znT],'delimiter','\t','precision',4)
+    fclose(fileID_write);
+else
+    Times_back = dataArray_mag_back{:, 1};
+    xnT_back = dataArray_mag_back{:, 2};
+    ynT_back = dataArray_mag_back{:, 3};
+    znT_back = dataArray_mag_back{:, 4};
+    dist_back = dataArray_dist_back{:, 1};
+    
+    xnT_m_back = zeros(size(xnT));
+    ynT_m_back = zeros(size(ynT));
+    znT_m_back = zeros(size(znT));
+    last_j = 1;
+    for i=1:size(xnT,1)
+        for j=last_j:size(xnT_back,1)
+            
+            cur_dist = abs(dist(i)- dist_back(j));
+            if (j==last_j)
+                best_dist = cur_dist;
+                best_j = j;
+            elseif(cur_dist<best_dist)
+                best_dist = cur_dist;
+                best_j = j ;
+            end
+            if (best_dist < max_dist_diff)
+                break
+            end
+
+        end   
+        last_j = best_j;
+        xnT_m_back(i) = xnT(i) - xnT_back(best_j);
+        ynT_m_back(i) = ynT(i) - ynT_back(best_j);
+        znT_m_back(i) = znT(i) - znT_back(best_j);
+    end
+
+    fileID_write = fopen(output_file,'w+');
+    dlmwrite(output_file,[dist xnT_m_back ynT_m_back znT_m_back xnT ...
+        ynT znT xnT_back ynT_back znT_back],'delimiter','\t','precision',4)
+    fclose(fileID_write);
+end
+
+%% Plotting
+figure
+if (background_exist == 0)
+    plot(dist,xnT,'r', 'LineWidth',1.5)
+    hold on
+    plot(dist,ynT,'LineWidth',1.5)
+    plot(dist,znT,'g','LineWidth',1.5)
+    legend('X', 'Y', 'Z')
+    ylabel('nT')
+    xlabel('Distance (arb.)')
+    set(gca,'XDir','Reverse')
+    hold off
+else
+    plot(dist,xnT_m_back,'r', 'LineWidth',1.5)
+    hold on
+    plot(dist,ynT_m_back,'LineWidth',1.5)
+    plot(dist,znT_m_back,'g','LineWidth',1.5)
+    axis tight
+    plot(dist,xnT_back, 'r--','LineWidth',0.5)
+    plot(dist,ynT_back,'b--','LineWidth',0.5)
+    plot(dist,znT_back, 'g--','LineWidth',0.5)
+    legend('X-X_b_a_c_k', 'Y-Y_b_a_c_k', 'Z-Z_b_a_c_k', ...
+        'X_b_a_c_k', 'Y_b_a_c_k', 'Z_b_a_c_k')
+    ylabel('nT')
+    xlabel('Distance (arb.)')
+    set(gca,'XDir','Reverse')
+    hold off
+end
+
+disp(['Code took : ' num2str(toc) 's'])
