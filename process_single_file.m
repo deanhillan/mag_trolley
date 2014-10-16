ceff40a75e35463b2c5f57e875bcbfa2afbdddde
dinob0t <dean.hillan@gmail.com>
2014-10-16 11:31:47 +1100

    fixed offsets

--- process_single_file.m
+++ process_single_file.m
@@ -11,8 +11,12 @@ filename_mag = 'data_single.Dat';
 % Input 2: Distance channel
 % Output file to write to
 output_file = 'combined_data.dat';
-% Track distance (m)
-track_dist = 3.5;
+% Track distance (m) to Z sensor
+track_dist = 1.14;
+% Y (first) sensor offset
+y_offset = 0.017;
+% X (thrid) sensor offset
+x_offset = -0.015;
 
 % Background signal information
 % Does background data exist (1=Yes, 0=No)
@@ -56,7 +60,47 @@ xnT = dataArray_mag{:, 2};
 ynT = dataArray_mag{:, 3};
 znT = dataArray_mag{:, 4};
 dist = dataArray_mag{:, 12};
+% Convert potentiometer data to a real distance
 dist_conv = -(-dist(1)+dist).*track_dist/(max(dist)-min(dist));
+% Interpolate to create x and y offset
+dist_conv_x = dist_conv + x_offset;
+dist_conv_y = dist_conv + y_offset;
+
+xnT_new = xnT;
+ynT_new = ynT;
+last_y = 1;
+last_x = 1;
+for i =1:length(dist_conv)
+    for j = 1:length(dist_conv_y)
+        cur_dist = abs(dist_conv(i)- dist_conv_y(j));
+            if (j==1)
+                best_dist = cur_dist;
+                best_j = j;
+            elseif(cur_dist<best_dist)
+                best_dist = cur_dist;
+                best_j = j ;
+            end   
+    end
+    last_y = best_j;
+    ynT_new(i) = ynT(best_j);
+
+    for j = 1:length(dist_conv_x)
+        cur_dist = abs(dist_conv(i)- dist_conv_x(j));
+            if (j==1)
+                best_dist = cur_dist;
+                best_j = j;
+            elseif(cur_dist<best_dist)
+                best_dist = cur_dist;
+                best_j = j ;
+            end     
+    end
+    last_x = best_j;
+    xnT_new(i) = xnT(best_j);
+end
+
+xnT = xnT_new;
+ynT = ynT_new;
+
 
 max_dist_diff = max(dist - [dist(2:size(dist,1)); dist(size(dist,1))]);
 
@@ -69,8 +113,48 @@ else
     xnT_back = dataArray_mag_back{:, 2};
     ynT_back = dataArray_mag_back{:, 3};
     znT_back = dataArray_mag_back{:, 4};
-    dist_back = dataArray_dist_back{:, 1};
+    dist_back = dataArray_mag_back{:, 12};
     
+    dist_back_conv = -(-dist_back(1)+dist_back).*track_dist/(max(dist_back)-min(dist_back));
+    dist_back_conv_x = dist_back_conv + x_offset;
+    dist_back_conv_y = dist_back_conv + y_offset;
+
+    xnT_back_new = xnT_back;
+    ynT_back_new = ynT_back;
+    last_y = 1;
+    last_x = 1;
+    for i =1:length(dist_back_conv)
+        for j = 1:length(dist_conv_y)
+            cur_dist = abs(dist_back_conv(i)- dist_back_conv_y(j));
+                if (j==1)
+                    best_dist = cur_dist;
+                    best_j = j;
+                elseif(cur_dist<best_dist)
+                    best_dist = cur_dist;
+                    best_j = j ;
+                end   
+        end
+        last_y = best_j;
+        ynT_back_new(i) = ynT_back(best_j);
+
+        for j = 1:length(dist_conv_x)
+            cur_dist = abs(dist_back_conv(i)- dist_back_conv_x(j));
+                if (j==1)
+                    best_dist = cur_dist;
+                    best_j = j;
+                elseif(cur_dist<best_dist)
+                    best_dist = cur_dist;
+                    best_j = j ;
+                end     
+        end
+        last_x = best_j;
+        xnT_back_new(i) = xnT_back(best_j);
+    end
+
+    xnT_back = xnT_back_new;
+    ynT_back = ynT_back_new;
+
+    % Background to sample interpolation
     xnT_m_back = zeros(size(xnT));
     ynT_m_back = zeros(size(ynT));
     znT_m_back = zeros(size(znT));
